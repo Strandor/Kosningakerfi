82870b78090b2702b9132b0d1c1354f3a41f7eaa
Steinar Þór Smári <steinar1smari@gmail.com>
2021-03-07 09:07:17 +0000

    Fixing ts error

--- src/utils.ts/secrets/index.ts
+++ src/utils.ts/secrets/index.ts
@@ -1,7 +1,2 @@
-const exitProcess = (message: string) => {
-  console.log(message);
-  process.exit(1);
-};
-
-export const DB_CONNECTION_URL: string = process.env["DB_CONNECTION_URL"];
-if (!DB_CONNECTION_URL) exitProcess("Missing DB_CONNECTION_URL");
+if (!process.env["DB_CONNECTION_URL"]) process.exit(1);
+export const DB_CONNECTION_URL = process.env["DB_CONNECTION_URL"];
