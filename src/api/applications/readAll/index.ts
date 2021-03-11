c9808a12a90218a33c143883d763e7042b886537
Steinar Þór Smari <steinar1smari@gmail.com>
2021-03-11 19:54:23 +0000

    Changing applications order

--- src/api/applications/readAll/index.ts
+++ src/api/applications/readAll/index.ts
@@ -2,15 +2,17 @@ import { Request, Response } from "express";
 import { Applications, ApplicationsCategory } from "../../../models";
 
 export default async (_req: Request, res: Response) => {
-  const doc = await ApplicationsCategory.findAll({
-    attributes: ["name"],
-    include: [
-      {
-        model: Applications,
-        attributes: ["id", "name"],
-      },
-    ],
-  });
+    const doc = await ApplicationsCategory.findAll({
+        attributes: ["name"],
+        order: [["name", "ASC"]],
+        include: [
+            {
+                model: Applications,
+                attributes: ["id", "name"],
+                order: [["name", "ASC"]],
+            },
+        ],
+    });
 
-  res.send(doc);
+    res.send(doc);
 };
