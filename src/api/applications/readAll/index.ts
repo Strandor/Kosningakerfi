42533d8ca176d1c03fc0a1a48604c30e4db9ea6e
Steinar Þór Smari <steinar1smari@gmail.com>
2021-03-11 20:05:54 +0000

    Fixing order in include

--- src/api/applications/readAll/index.ts
+++ src/api/applications/readAll/index.ts
@@ -4,12 +4,14 @@ import { Applications, ApplicationsCategory } from "../../../models";
 export default async (_req: Request, res: Response) => {
     const doc = await ApplicationsCategory.findAll({
         attributes: ["name"],
-        order: [["name", "ASC"]],
+        order: [
+            ["name", "ASC"],
+            [Applications, "name", "ASC"],
+        ],
         include: [
             {
                 model: Applications,
                 attributes: ["id", "name"],
-                order: [["name", "ASC"]],
             },
         ],
     });
