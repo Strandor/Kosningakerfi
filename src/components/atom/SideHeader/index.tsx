bf0435bc5902ac331024e99afb9f42618b15499a
Steinar Þór Smari <steinar1smari@gmail.com>
2021-03-12 16:44:37 +0000

    Fixnig type error

--- src/components/atom/SideHeader/index.tsx
+++ src/components/atom/SideHeader/index.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import React from "react";
-import styles from "./Sideheader.module.css";
+import styles from "./SideHeader.module.css";
 import { IProps } from "./interface";
 
 export const SideHeader = ({ children, links }: IProps) => {
-  return (
-    <div className={styles.outer}>
-      <div className={styles.header}>
-        {links.map((link) => (
-          <Link href={link.link}>
-            <p>{link.name}</p>
-          </Link>
-        ))}
-      </div>
-      <div className={styles.children}>{children}</div>
-    </div>
-  );
+    return (
+        <div className={styles.outer}>
+            <div className={styles.header}>
+                {links.map((link) => (
+                    <Link href={link.link}>
+                        <p>{link.name}</p>
+                    </Link>
+                ))}
+            </div>
+            <div className={styles.children}>{children}</div>
+        </div>
+    );
 };
